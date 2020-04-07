a770f67ffe9fd109572a282a88ebe02873c251ac
Fatroul <greensgoldberg@gmail.com>
2020-04-07 17:15:57 +0200

    exo2

--- fichier.php
+++ fichier.php
@@ -1,22 +1,9 @@
 <?php 
 
-$file=fopen("maj.txt","r");
-$maj=fgets($file);
-fclose($file);
-echo "<p>Mis à jour le $maj</p>";
+$file=fopen("count.txt","c+");
+$line=fgets($file);
+var_dump($line);
 
-/*$count=fopen("count.txt","r+");
-//fwrite($count,"1");
-
-fwrite($count,"1");
-
-$str="";
-while($str!==false) {
-    fseek($count, 0);
-    fwrite($count,"$str=+1");
-    $str=fgets($count);
-    var_dump($str);
-}
-
-fclose($count);
-*/
+$count=($line)?$line+1:1;
+fseek($file,0);
+fwrite($file,$count);
